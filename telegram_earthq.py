44d1e4c9efcc1f5c1475b4c9e88f6d761c56138d
hyperjino <hyperjino@gmail.com>
2016-10-24 17:32:59 +0900

    earthqtest2

--- telegram_earthq.py
+++ telegram_earthq.py
@@ -51,7 +51,7 @@ def earthq():
     return result
  
 while True:
-    time.sleep(30)
+    time.sleep(10)
     result = earthq()
     if result[0] == 0:
         print('오늘자 관측없음')
@@ -64,6 +64,7 @@ while True:
         
     elif lasttime == result[0]:
         print "다른점 없음"
+        bot.sendMessage('18311619', "[지진안내]\n%s\n지역: %s\n진도: %s\n원인: %s" %(result[0], result[1], result[2], result[3]))
         continue
     
     
